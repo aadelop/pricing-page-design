c489788b4540281a9fc5a402b36fdf54f6598ce7
Alfredo Delgado L <aadelop@gmail.com>
2024-09-20 09:38:36 +0200

    Added clean and initial source files project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Design project</h1>
+  </StrictMode>
+)
