6feb0ee92da50b248ca025c1fdf3208a3292843e
Alfredo Delgado L <aadelop@gmail.com>
2024-09-20 11:57:17 +0200

    Added initial Header structure and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,16 @@
+import { Logo } from "./Logo";
+
 export function Header(){
-    return <div> Header </div>
+    return <div className="my-2 text-dark d-flex justify-content-between">
+                <div className="d-flex">
+                    <Logo></Logo>
+                    <p>My Company</p>
+                </div>
+              
+                <div >
+                    <a  className="mx-3 text-decoration-none" href="#">blog</a>
+                    <a className="text-decoration-none" href="#">youtube</a>
+                </div>
+                
+    </div> 
 }
